b464c9c2032a24177502be70535cdf1861574ebc
Alejandro <159506396+YahirArboleya@users.noreply.github.com>
2024-09-28 05:43:05 +0000

    reset

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,30 @@
 <script setup>
-  import {ref} from 'vue';
-  //Modelo
-  const header = ref('App lista de compras');
-  const items = ref([
-    {id: '0', label:'10 Bolillos'},
-    {id: '1', label:'1 Lata de frijoles'},
-    {id: '2', label:'1 Chelas'},
-    {id: '3', label:'1 Nutella'}
-  ]);
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { ref } from "vue";
 
-
-  const newItem = ref('');
-  const newItemHighPriority = ref(false);
-  const iceCreamFlavors = ref([]);
-
+const header = ref("🛒 Shopping List App");
+const items = ref([
+  { id: 1, label: "10 bolillos" },
+  { id: 2, label: "1 lata de frijoles" },
+  { id: 3, label: "2 lata de atún" },
+]);
+const newItem = ref("");
+const newItemHighPriority = ref(false);
 </script>
 
 <template>
-  <h1>
-    <i class="material-icons shopping-cart-icon">local_mall</i>
-    {{ header }} 
-  </h1>
-
-  <input type="text" placeholder="Add Item" v-model.trim="newItem">
-  <!-- Radio Buttons -->
-  <label><input type="checkbox" v-model="newItemHighPriority">Alta Prioridad</label>
-  {{ newItemHighPriority }}
-
-  <!-- Helados -->
+  <h1>{{ header }}</h1>
+  <input v-model.trim="newItem" type="text" placeholder="Add Item" />
+  <!-- Caja de seleccion de prioridad -->
   <label>
-		<input type="checkbox" v-model="iceCreamFlavors" value="vanilla">
-		Vanilla
-	</label>
-  <label>
-		<input type="checkbox" v-model="iceCreamFlavors" value="chocolate">
-		Chocolate
-	</label>
-  <label>
-		<input type="checkbox" v-model="iceCreamFlavors" value="strawnerry">
-		Strawberry
-	</label>
-  {{iceCreamFlavors}}
+    <input type="checkbox" v-model="newItemHighPriority">
+    High Priority
+  </label>
   <!-- Lista -->
   <ul>
-
+    <li v-for="({ id, label }, index) in items" key="id">⚜ {{ label }}</li>
   </ul>
-
-
-  <ul>
-    <li v-for="item in items" v-bind:key="item.id">  🛒 {{ item.label }} </li>
-  </ul>
-
 </template>
 
 <style scoped>
