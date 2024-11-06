1d984ac98ba9a27012793bf83249ad241516f911
Yahir <yahir2_telas@outlook.com>
2024-11-06 12:49:10 -0600

    FEAT: ⭐Property

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
+  import { ref } from 'vue';
   import planItem from './components/plan-item.vue';
+  const plans = ref(['El cafetero', 'El chelero', 'El Choro']);
 </script>
 
 <template>
@@ -18,9 +20,7 @@
     </h2>
 
     <div class="plans">
-      <planItem></planItem>
-      <planItem></planItem>
-      <planItem></planItem>      
+      <planItem  v-for = "plan in plans" :key="plan" :name="plan"></planItem>    
     </div>
   </div>
 </template>
--- src/components/plan-item.vue
+++ src/components/plan-item.vue
@@ -2,13 +2,23 @@
   <div class="plan">
     <div class="description">
       <span class="title">
-        El adicto
+        {{ name }}
       </span>
     </div>
   </div>
 </template>
 
 <script setup>
+  //usando un macro
+  //defenir las props
+  //defineProps(['name','planIcon']);
+  defineProps({
+    name: {
+      type: String,
+      required: true
+    }
+  })
+
 
 </script>
 
