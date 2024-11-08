0c2dc1a5e09db95bb62d56f699bb16943c46d65d
Yahir <yahir2_telas@outlook.com>
2024-11-08 10:45:24 -0600

    FEAT ❤ Rename Component PlanItem

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,12 +1,12 @@
 <script setup>
-    import planItem from './plan-item.vue';
+    import PlanItem from './PlanItem.vue';
     import { ref } from 'vue';
     const plans = ref(['El Cafetero', 'El Chelero', 'El Choro']);
 </script>
 
 <template>
     <div class="plans">
-      <planItem  v-for = "plan in plans" :key="plan" :name="plan"></planItem>    
+      <PlanItem  v-for = "plan in plans" :key="plan" :name="plan"></PlanItem>    
     </div>
 </template>
 
