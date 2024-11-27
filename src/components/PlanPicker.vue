0810eca5032804df3965b7dbbd9395f717a67e3c
Yahir <yahir2_telas@outlook.com>
2024-11-27 11:27:04 -0600

    ⭐FEAT: HOOKS Topic Template References

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -12,18 +12,22 @@
     const selectPlan = (name) =>{
       selectedPlan.value = name;
     };
+
+    const plansWrapper = ref(null);
+
     //intentando obtener la referencia al elemento .plans
     console.log("Termina de ejecutar el script setup");
 
     //registrando el took mounted
+    //tooks: funciones que se ejecutan cuando el ciclo de vida de un componenete cambia
     onMounted(()=>{
       console.log("El componente PlanPicker se ha montado")
-      console.log(document.querySelector('plans'))
+      console.log(plansWrapper.value);
     });
 </script>
 
 <template>
-    <div class="plans">
+    <div ref="plansWrapper" class="plans">
       <PlanPickerItem  
       v-for = "plan in plans" 
       :key="plan" 
