778383a0c4f120e272e3fa916c4f50dfa1220b54
Yahir <yahir2_telas@outlook.com>
2024-11-29 08:59:33 -0600

    ⭐FEAt: Slots 2

--- src/components/FancyButton.vue
+++ src/components/FancyButton.vue
@@ -1,10 +1,24 @@
 <template>
-    <button>Hazme Clic!</button>
+    <button 
+      @mouseenter="hover = true"
+      @mouseleave="hover = false"> 
+      <slot name="icon" :hover="hover">➡️</slot>
+      <slot>Submit</slot>
+    </button>
   </template>
   
   <script setup>
+  import { ref } from 'vue';
+  const hover = ref(false);
   </script>
-  
+
   <style scoped>
-  
+  .content{
+    text-align: center;
+  }
+  button{
+    width: 12rem;
+    margin: 30px auto;
+    font-size: 2rem;
+  }
   </style>
